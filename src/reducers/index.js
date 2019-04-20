72402803d4264b1ada41bba436b1bacc2af730c1
Hieu <hieudoan2609@gmail.com>
2019-04-20 17:41:48 +0700

    add Login tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,20 @@
 import { combineReducers } from "redux";
+
 import accountReducer from "./accountReducer";
 import appReducer from "./appReducer";
+import { RESET_INITIAL_STATE } from "../actions/types";
 
-export default combineReducers({
+const indexReducer = combineReducers({
   account: accountReducer,
   app: appReducer
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === RESET_INITIAL_STATE) {
+    state = undefined;
+  }
+
+  return indexReducer(state, action);
+};
+
+export default rootReducer;
