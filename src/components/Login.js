fc713ceb1279019d6e884ab5b82f0f1eae9b06ef
Hieu <hieudoan2609@gmail.com>
2019-04-20 20:39:05 +0700

    refactor login

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,16 +7,14 @@ import "./Login.scss";
 import Button from "./Button";
 
 class Login extends Component {
-  render() {
+  renderBody = () => {
     if (this.props.account.loading) {
       return (
-        <div className={`Login card text-center ${this.props.app.theme}`}>
-          <Loading
-            active={this.props.account.loading}
-            type="absolute"
-            theme={this.props.app.theme}
-          />
-        </div>
+        <Loading
+          active={this.props.account.loading}
+          type="absolute"
+          theme={this.props.app.theme}
+        />
       );
     }
 
@@ -31,76 +29,74 @@ class Login extends Component {
       );
 
       return (
-        <div className={`Login card text-center ${this.props.app.theme}`}>
-          <div className="card-body">
-            <h5 className="card-title">Welcome!</h5>
-            <p className="card-text">
-              You have now logged in. Your address is 0x
-              {address_first_four_digits}...{address_last_four_digits}
-            </p>
-            <Button
-              theme={this.props.app.theme}
-              onClick={this.props.accountLogout}
-            >
-              Disconnect
-            </Button>
-          </div>
+        <div className="card-body">
+          <h5 className="card-title">Welcome!</h5>
+          <p className="card-text">
+            You have now logged in. Your address is 0x
+            {address_first_four_digits}...{address_last_four_digits}
+          </p>
+          <Button
+            theme={this.props.app.theme}
+            onClick={this.props.accountLogout}
+          >
+            Disconnect
+          </Button>
         </div>
       );
     }
 
     if (this.props.account.metamask === "unavailable") {
       return (
-        <div className={`Login card text-center ${this.props.app.theme}`}>
-          <div className="card-body">
-            <h5 className="card-title">MetaMask is not installed</h5>
-            <p className="card-text">Would you like to install it now?</p>
-            <a
-              href="https://metamask.io"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button theme={this.props.app.theme}>Install MetaMask</Button>
-            </a>
-          </div>
+        <div className="card-body">
+          <h5 className="card-title">MetaMask is not installed</h5>
+          <p className="card-text">Would you like to install it now?</p>
+          <a
+            href="https://metamask.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button theme={this.props.app.theme}>Install MetaMask</Button>
+          </a>
         </div>
       );
     }
 
     if (this.props.account.metamask === "wrongnetwork") {
       return (
-        <div className={`Login card text-center ${this.props.app.theme}`}>
-          <div className="card-body">
-            <h5 className="card-title">Wrong network detected</h5>
-            <p className="card-text">
-              Please set MetaMask's network to {this.props.app.networkName} and
-              try again.
-            </p>
-            <Button
-              onClick={this.props.accountLoginAsync}
-              theme={this.props.app.theme}
-            >
-              Try again
-            </Button>
-          </div>
-        </div>
-      );
-    }
-
-    return (
-      <div className={`Login card text-center ${this.props.app.theme}`}>
         <div className="card-body">
-          <h5 className="card-title">You are not logged in</h5>
+          <h5 className="card-title">Wrong network detected</h5>
           <p className="card-text">
-            Connect to MetaMask to manage your account.
+            Please set MetaMask's network to {this.props.app.networkName} and
+            try again.
           </p>
           <Button
             onClick={this.props.accountLoginAsync}
             theme={this.props.app.theme}
           >
-            Connect
+            Try again
           </Button>
         </div>
+      );
+    }
+
+    return (
+      <div className="card-body">
+        <h5 className="card-title">You are not logged in</h5>
+        <p className="card-text">Connect to MetaMask to manage your account.</p>
+        <Button
+          onClick={this.props.accountLoginAsync}
+          theme={this.props.app.theme}
+        >
+          Connect
+        </Button>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div className={`Login card text-center ${this.props.app.theme}`}>
+        {this.renderBody()}
       </div>
     );
   }
