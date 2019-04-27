e8256f52ec6c4bac7a76c4bf2c66c5782c1f5f73
Hieu <hieudoan2609@gmail.com>
2019-04-27 16:42:25 +0700

    add modal animation

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { CSSTransition } from "react-transition-group";
 
 import "./Modal.scss";
 
@@ -19,40 +20,45 @@ class Modal extends Component {
 
   render() {
     return (
-      <div
-        className={`Modal ${this.props.theme} ${this.props.show ? "show" : ""}`}
+      <CSSTransition
+        in={this.props.show}
+        timeout={{ enter: 0, exit: 300 }}
+        className={`Modal ${this.props.theme}`}
+        unmountOnExit
       >
-        <div className="modal">
-          <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Deposit Ethereum (ETH)</h5>
-                <div className="close" onClick={this.props.onHide}>
-                  <ion-icon name="close-circle" />
-                </div>
-              </div>
-
-              <div className="modal-body">
-                <div className="input-group">
-                  <input
-                    className="form-control"
-                    placeholder="Amount"
-                    spellCheck="false"
-                  />
-                  <div className="input-group-append">
-                    <span className="input-group-text">DEPOSIT</span>
+        <div>
+          <div className="modal">
+            <div className="modal-dialog modal-dialog-centered">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Deposit Ethereum (ETH)</h5>
+                  <div className="close" onClick={this.props.onHide}>
+                    <ion-icon name="close-circle" />
                   </div>
                 </div>
-                <small className="form-text text-muted">
-                  Deposit entire balance
-                </small>
+
+                <div className="modal-body">
+                  <div className="input-group">
+                    <input
+                      className="form-control"
+                      placeholder="Amount"
+                      spellCheck="false"
+                    />
+                    <div className="input-group-append">
+                      <span className="input-group-text">DEPOSIT</span>
+                    </div>
+                  </div>
+                  <small className="form-text text-muted">
+                    Deposit entire balance
+                  </small>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="backdrop" onClick={this.props.onHide} />
+            <div className="backdrop" onClick={this.props.onHide} />
+          </div>
         </div>
-      </div>
+      </CSSTransition>
     );
   }
 }
