9b5131424ed2307d84eeb2ab9f70d4f174eb48ed
Hieu <hieudoan2609@gmail.com>
2019-06-13 16:19:36 +0700

    prevent orderbook loading if currentMarket is not set

--- src/actions/orderBookActions.js
+++ src/actions/orderBookActions.js
@@ -7,10 +7,14 @@ import config from '../config'
 
 export const orderBookLoadAsync = () => {
   return async (dispatch, getState) => {
-    // const { market } = getState()
-    // const { API_HTTP_ROOT } = config;
+    const { market } = getState()
+    const { API_HTTP_ROOT } = config;
 
-    // const orderBooksResponse = await axios.get(`${API_HTTP_ROOT}/order_books/${market.currentMarket}?per_page=1000`)
+    if (!market.currentMarket) {
+      return
+    }
+
+    const orderBooksResponse = await axios.get(`${API_HTTP_ROOT}/order_books/${market.currentMarket}?per_page=1000`)
     
     // console.log(orderBooksResponse)
   };
