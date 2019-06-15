9e40a932523890f9dcff29bc558102807cdf2fde
Hieu <hieudoan2609@gmail.com>
2019-06-15 16:17:53 +0700

    marketLoadAsync load default market if provided market is invalid

--- src/actions/marketActions.js
+++ src/actions/marketActions.js
@@ -8,10 +8,12 @@ export const marketLoadAsync = () => {
     dispatch({
       type: MARKET_LOADING
     })
-    const { tickers } = getState()
+    const { tickers, markets } = getState()
     const defaultTicker = tickers.all[0]
     const match = matchPath(window.location.pathname, { path: '/market/:marketSymbol' })
-    const currentMarket = match ? match.params.marketSymbol : defaultTicker.marketSymbol
+    const marketSymbol = match ? match.params.marketSymbol : undefined
+    const marketSymbolExists = markets.all.filter(m => m.symbol === marketSymbol)[0] ? true : false
+    const currentMarket = marketSymbolExists ? marketSymbol : defaultTicker.marketSymbol
     const baseSymbol = currentMarket.split("_")[0] || ""
     const quoteSymbol = currentMarket.split("_")[1] || ""
     await dispatch(orderBookLoadAsync(currentMarket))
