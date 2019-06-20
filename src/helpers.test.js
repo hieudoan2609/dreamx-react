4dc9c1fc4fbe1c45448dd09c3194752304f186ed
Hieu <hieudoan2609@gmail.com>
2019-06-20 09:10:27 +0700

    use generateOrders to improve tests readability

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,20 +1,21 @@
-import { matchBuyOrders, matchSellOrders } from './helpers'
 import * as Web3Utils from 'web3-utils'
 
-const baseAddress = "0x0000000000000000000000000000000000000000"
-const quoteAddress = "0xe62cc4212610289d7374f72c2390a40e78583350"
+import { matchBuyOrders, matchSellOrders } from './helpers'
+import { generateOrders } from './testHelpers'
 
 describe("matchBuyOrders", () => {
   describe("when there are no matched orders", () => {
     test.only("return the submitted order", () => {
-      const buyBook = [
-        { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1'), type: 'buy' },
-        { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.1'), type: 'buy' },
-        { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.2'), type: 'buy' },
-        { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.3'), type: 'buy' },
-        { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.4'), type: 'buy' }
-      ]
-      const order = { giveTokenAddress: quoteAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: baseAddress, takeAmount: Web3Utils.toWei('1.1'), type: 'sell' }
+      const buyBook = generateOrders([
+        { type: 'buy', price: '0.9', amount: '1', date: "2019-06-19T20:25:59.459Z" },
+        { type: 'buy', price: '0.8', amount: '1', date: "2019-06-19T20:25:59.459Z" },
+        { type: 'buy', price: '0.7', amount: '1', date: "2019-06-19T20:25:59.459Z" },
+        { type: 'buy', price: '0.6', amount: '1', date: "2019-06-19T20:25:59.459Z" },
+        { type: 'buy', price: '0.5', amount: '1', date: "2019-06-19T20:25:59.459Z" }
+      ])
+      const order = generateOrders([
+        { type: 'sell', price: '1', amount: '1', date: "2019-06-19T20:25:59.459Z" }
+      ])[0]
       const expectedMatchResults = [ order ]
       const receivedMatchResults = matchBuyOrders({ order, buyBook })
       expect(receivedMatchResults).toEqual(expectedMatchResults)
@@ -23,14 +24,17 @@ describe("matchBuyOrders", () => {
 
   describe("when orderbook doesn't enough volume", () => {
     test("match all orders, return trades and the remaining order", () => {
-      const buyBook = [
-        { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1'), type: 'buy' },
-        { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.1'), type: 'buy' },
-        { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.2'), type: 'buy' },
-        { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.3'), type: 'buy' },
-        { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.4'), type: 'buy' }
-      ]
-      const order = { giveTokenAddress: quoteAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: baseAddress, takeAmount: Web3Utils.toWei('1.1'), type: 'sell' }
+      // const buyBook = [
+      //   { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1'), type: 'buy' },
+      //   { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.1'), type: 'buy' },
+      //   { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.2'), type: 'buy' },
+      //   { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.3'), type: 'buy' },
+      //   { giveTokenAddress: baseAddress, giveAmount: Web3Utils.toWei('1'), takeTokenAddress: quoteAddress, takeAmount: Web3Utils.toWei('1.4'), type: 'buy' }
+      // ]
+      // const order = { giveTokenAddress: quoteAddress, giveAmount: Web3Utils.toWei('3.3'), takeTokenAddress: baseAddress, takeAmount: Web3Utils.toWei('3'), type: 'sell' }
+      // const expectedMatchResults = [
+      //   { giveTokenAddress: quoteAddress, giveAmount: Web3Utils.toWei('3.3'), takeTokenAddress: baseAddress, takeAmount: Web3Utils.toWei('1.2'), type: 'sell' }
+      // ]
     });
   })
 
