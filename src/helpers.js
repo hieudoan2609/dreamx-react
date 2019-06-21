d3533492af29bb710c66cacc4d983b2a6ea13310
Hieu <hieudoan2609@gmail.com>
2019-06-21 08:47:22 +0700

    remove undefined order attributes from generateTestOrders result

--- src/helpers.js
+++ src/helpers.js
@@ -277,11 +277,28 @@ export const matchSellOrders = ({ price, amount, sellBook, baseAddress, quoteAdd
 }
 
 export const generateTestOrders = (orders) => {
+  const defaultOrder = { 
+    type: 'sell',
+    price: '0',
+    amount: '0',
+    filled: '0',
+    createdAt: '2019-06-19T20:25:59.459Z',
+    orderHash: 'SELL#0'
+  }
   const baseAddress = "0x0000000000000000000000000000000000000000"
   const quoteAddress = "0xe62cc4212610289d7374f72c2390a40e78583350"
   orders = orders.map(order => {
+    const emptyAttrs = []
+    // initialize undefined attrs with default ones
+    for (let key of Object.keys(defaultOrder)) {
+      if (!order[key]) {
+        order[key] = defaultOrder[key]
+        emptyAttrs.push(key)
+      }
+    }
     order.amount = Web3Utils.toBN(Web3Utils.toWei(order.amount))
     order.price = Web3Utils.toBN(Web3Utils.toWei(order.price))
+    order.filled = Web3Utils.toBN(Web3Utils.toWei(order.filled))
     let giveTokenAddress, giveAmount, takeTokenAddress, takeAmount
     if (order.type === 'buy') {
       giveTokenAddress = baseAddress
@@ -294,18 +311,14 @@ export const generateTestOrders = (orders) => {
       takeTokenAddress = baseAddress
       takeAmount = order.amount.mul(order.price).div(oneEther)
     }
-    giveAmount = giveAmount.toString()
-    takeAmount = takeAmount.toString()
     const createdAt = order.date
     const orderHash = order.orderHash
     const type = order.type
-    const filled = order.filled
-    order = { giveTokenAddress, giveAmount, takeTokenAddress, takeAmount, createdAt, orderHash, type, filled }
-    if (!order.orderHash) {
-      // orderHash doesn't exist, this order is part of a match engine response, make it look like one
-      delete order.orderHash
-      delete order.createdAt
-      delete order.filled
+    const filled = calculateGiveAmount(order.filled, giveAmount, takeAmount)
+    order = { giveTokenAddress, giveAmount: giveAmount.toString(), takeTokenAddress, takeAmount: takeAmount.toString(), createdAt, orderHash, type, filled: filled.toString() }
+    // remove undefined attrs
+    for (let key of emptyAttrs) {
+      delete order[key]
     }
     return order
   })
--- src/helpers.test.js
+++ src/helpers.test.js
@@ -3,54 +3,61 @@ import * as Web3Utils from 'web3-utils'
 import { matchBuyOrders, matchSellOrders, generateTestOrders, generateTestTrades } from './helpers'
 
 describe("matchBuyOrders", () => {
-  describe("when there are no matched orders", () => {
-    test("return the submitted order", () => {
-      const buyBook = generateTestOrders([
-        { type: 'buy', price: '0.9', amount: '1', date: "2019-06-19T20:25:59.459Z", orderHash: "BUY#0" },
-        { type: 'buy', price: '0.8', amount: '1', date: "2019-06-19T20:25:59.459Z", orderHash: "BUY#1" },
-        { type: 'buy', price: '0.7', amount: '1', date: "2019-06-19T20:25:59.459Z", orderHash: "BUY#2" },
-        { type: 'buy', price: '0.6', amount: '1', date: "2019-06-19T20:25:59.459Z", orderHash: "BUY#3" },
-        { type: 'buy', price: '0.5', amount: '1', date: "2019-06-19T20:25:59.459Z", orderHash: "BUY#4" }
-      ])
-      const order = generateTestOrders([
-        { type: 'sell', price: '1', amount: '1' }
-      ])[0]
-      const expectedMatchResults = { trades: [], orders: [order] }
-      const receivedMatchResults = matchBuyOrders({ order, buyBook })
-      expect(receivedMatchResults).toEqual(expectedMatchResults)
-    })
+  test("return the submitted order when there are no matched orders", () => {
+    const buyBook = generateTestOrders([
+      { type: 'buy', price: '0.9', amount: '1', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#0" },
+      { type: 'buy', price: '0.8', amount: '1', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#1" },
+      { type: 'buy', price: '0.7', amount: '1', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#2" },
+      { type: 'buy', price: '0.6', amount: '1', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#3" },
+      { type: 'buy', price: '0.5', amount: '1', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#4" }
+    ])
+    const order = generateTestOrders([
+      { type: 'sell', price: '1', amount: '1' }
+    ])[0]
+    const expectedMatchResults = { trades: [], orders: [order] }
+    const receivedMatchResults = matchBuyOrders({ order, buyBook })
+    expect(receivedMatchResults).toEqual(expectedMatchResults)
   })
 
-  describe("when orderbook doesn't enough volume", () => {
-    test("match all orders, return trades and a rest order", () => {
-      const buyBook = generateTestOrders([
-        { type: 'buy', price: '0.9', amount: '1', filled: '0', date: "2019-06-19T20:25:59.459Z", orderHash: "BUY#0" },
-        { type: 'buy', price: '0.8', amount: '1', filled: '0', date: "2019-06-19T20:25:59.459Z", orderHash: "BUY#1" },
-        { type: 'buy', price: '0.7', amount: '1', filled: '0', date: "2019-06-19T20:25:59.459Z", orderHash: "BUY#2" },
-        { type: 'buy', price: '0.6', amount: '1', filled: '0', date: "2019-06-19T20:25:59.459Z", orderHash: "BUY#3" },
-        { type: 'buy', price: '0.5', amount: '1', filled: '0', date: "2019-06-19T20:25:59.459Z", orderHash: "BUY#4" }
-      ])
-      const order = generateTestOrders([
-        { type: 'sell', price: '0.8', amount: '3' }
-      ])[0]
-      const expectedTrades = generateTestTrades([
-        { orderHash: 'BUY#0', amount: '0.9' },
-        { orderHash: 'BUY#1', amount: '0.8' }
-      ])
-      const expectedOrders = generateTestOrders([
-        { type: 'sell', price: '0.8', amount: '1' }
-      ])
-      const expectedMatchResults = { trades: expectedTrades, orders: expectedOrders }
-      const receivedMatchResults = matchBuyOrders({ order, buyBook })
-      expect(receivedMatchResults).toEqual(expectedMatchResults)
-    });
+  test("match all orders, return trades and a rest order", () => {
+    const buyBook = generateTestOrders([
+      { type: 'buy', price: '0.9', amount: '1', filled: '0', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#0" },
+      { type: 'buy', price: '0.8', amount: '1', filled: '0', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#1" },
+      { type: 'buy', price: '0.7', amount: '1', filled: '0', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#2" },
+      { type: 'buy', price: '0.6', amount: '1', filled: '0', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#3" },
+      { type: 'buy', price: '0.5', amount: '1', filled: '0', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#4" }
+    ])
+    const order = generateTestOrders([
+      { type: 'sell', price: '0.8', amount: '3' }
+    ])[0]
+    const expectedTrades = generateTestTrades([
+      { orderHash: 'BUY#0', amount: '0.9' },
+      { orderHash: 'BUY#1', amount: '0.8' }
+    ])
+    const expectedOrders = generateTestOrders([
+      { type: 'sell', price: '0.8', amount: '1' }
+    ])
+    const expectedMatchResults = { trades: expectedTrades, orders: expectedOrders }
+    const receivedMatchResults = matchBuyOrders({ order, buyBook })
+    expect(receivedMatchResults).toEqual(expectedMatchResults)
+  });
 
-    test("match a partially filled order, return a trade and a rest order", () => {})
-  })
+  // test("match a partially filled order, return a trade and a rest order", () => {
+  //   const buyBook = generateTestOrders([
+  //     { type: 'buy', price: '0.9', amount: '1', filled: '0.5', createdAt: "2019-06-19T20:25:59.459Z", orderHash: "BUY#0" },
+  //   ])
+  //   const order = generateTestOrders([
+  //     { type: 'sell', price: '0.8', amount: '3' }
+  //   ])[0]
+  //   // const expectedTrades = generateTestTrades([
+  //   //   { orderHash: 'BUY#0', amount: '0.45' }
+  //   // ])
+  //   // const expectedOrders = generateTestOrders([
+  //   //   { type: 'sell', price: '0.8', amount: '2.5' }
+  //   // ])
+  // })
 
-  describe("when orderbook has enough volume", () => {
-    test("orders with higher prices should be matched first", () => {});
+  test("orders with higher prices should be matched first", () => {});
 
-    test("if there are multiple orders of the same price, the older ones should be matched first", () => {})
-  })
+  test("if there are multiple orders of the same price, the older ones should be matched first", () => {})
 })
