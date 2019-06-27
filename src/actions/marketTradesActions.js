cd85003f5be93356632cb9ab7a3aa9c1ce523928
Hieu <hieudoan2609@gmail.com>
2019-06-27 07:05:33 +0700

    add live data for marketTradesReducer

--- src/actions/marketTradesActions.js
+++ src/actions/marketTradesActions.js
@@ -4,15 +4,19 @@ import {
   MARKET_TRADES_LOAD
 } from "../actions/types";
 import config from '../config'
+import { processTrade } from '../helpers'
 
-export const marketTradesLoadAsync = () => {
-  return async dispatch => {
-    // const { API_HTTP_ROOT } = config;
-    // const marketTradesReponse = await axios.get(`${API_HTTP_ROOT}/trades/${marketSymbol}?per_page=1000`)
+export const marketTradesLoadAsync = (marketSymbol) => {
+  return async (dispatch, getState) => {
+    const { API_HTTP_ROOT } = config;
+
+    const marketTradesResponse = await axios.get(`${API_HTTP_ROOT}/trades?market_symbol=${marketSymbol}`)
+    const marketTrades = marketTradesResponse.data.records.map(t => processTrade(getState, t))
+    
     dispatch({
       type: MARKET_TRADES_LOAD,
       payload: {
-        all: []
+        all: marketTrades
       }
     })
   };
