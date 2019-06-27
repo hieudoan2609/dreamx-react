d0b6bd2551b4991c30fbc22867eaaa11123ce064
Hieu <hieudoan2609@gmail.com>
2019-06-27 08:56:15 +0700

    add socket events for TradeHistory

--- src/actions/marketTradesActions.js
+++ src/actions/marketTradesActions.js
@@ -5,6 +5,7 @@ import {
 } from "../actions/types";
 import config from '../config'
 import { processTrade } from '../helpers'
+import singletons, { setSingleton } from "../singletons";
 
 export const marketTradesLoadAsync = (marketSymbol) => {
   return async (dispatch, getState) => {
@@ -13,11 +14,55 @@ export const marketTradesLoadAsync = (marketSymbol) => {
     const marketTradesResponse = await axios.get(`${API_HTTP_ROOT}/trades?market_symbol=${marketSymbol}`)
     const marketTrades = marketTradesResponse.data.records.map(t => processTrade(getState, t))
     
+    dispatch(initializeCableSubscriptions(marketSymbol))
+
     dispatch({
       type: MARKET_TRADES_LOAD,
-      payload: {
-        all: marketTrades
-      }
+      payload: { all: marketTrades }
     })
   };
 };
+
+const initializeCableSubscriptions = marketSymbol => {
+  return dispatch => {
+    const { cable } = singletons;
+
+    const marketTradesSubscription = cable.subscriptions.create(
+      { channel: "MarketTradesChannel", market_symbol: marketSymbol },
+      {
+        connected: () => {},
+        received: data => {
+          dispatch(updateMarketTradesAsync(data.payload));
+        }
+      }
+    );
+
+    setSingleton("MarketTradesChannel", marketTradesSubscription);
+  };
+};
+
+const updateMarketTradesAsync = (newTrades) => {
+  if (!Array.isArray(newTrades)) {
+    newTrades = [newTrades];
+  }
+
+  return async (dispatch, getState) => {
+    const { marketTrades } = getState();
+
+    const updatedTrades = [];
+    for (let newTrade of newTrades) {
+      updatedTrades.push(processTrade(getState, newTrade))
+    }
+    for (let trade of marketTrades.all) {
+      const newTrade = updatedTrades.filter(t => t.uuid === trade.uuid)[0]
+      if (!newTrade) {
+        updatedTrades.push(trade)
+      }
+    }
+
+    dispatch({
+      type: MARKET_TRADES_LOAD,
+      payload: { all: updatedTrades }
+    });
+  };
+}
