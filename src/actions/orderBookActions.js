bd0c23e0ea93403118a53f3470fe4f2521820e52
Hieu <hieudoan2609@gmail.com>
2020-01-28 23:44:47 +0700

    fix orderbook total calculation

--- src/actions/orderBookActions.js
+++ src/actions/orderBookActions.js
@@ -10,48 +10,12 @@ import singletons, { setSingleton } from "../singletons";
 
 export const orderBookLoadAsync = (marketSymbol) => {
   return async (dispatch, getState) => {
-    const { app } = getState()
-    const takerMinimum = Web3Utils.toBN(app.takerMinimum)
     const { API_HTTP_ROOT } = config;
     const orderBooksResponse = await axios.get(`${API_HTTP_ROOT}/order_books/${marketSymbol}?per_page=1000`)
-    let buyBook = []
-    let sellBook = []
-    let totalBuy = Web3Utils.toBN(0)
-    let totalSell = Web3Utils.toBN(0)
-    for (let order of orderBooksResponse.data.bid.records) {
-      order = processOrder(getState, order)
-      const takeAmount = Web3Utils.toBN(order.takeAmount)
-      const giveAmount = Web3Utils.toBN(order.giveAmount)
-      const filled = Web3Utils.toBN(order.filled)
-      const filledTake = calculateTakeAmount(filled, giveAmount, takeAmount)
-      const remainingTakeAmount = takeAmount.sub(filledTake)
-      const remainingGiveAmount = giveAmount.sub(filled)
-      // skip this order if its remaining volume doesn't meet taker's minimum
-      if (remainingGiveAmount.lt(takerMinimum)) {
-        continue
-      }
-      totalBuy = totalBuy.add(remainingTakeAmount)
-      buyBook.push(order)
-    }
-    for (let order of orderBooksResponse.data.ask.records) {
-      order = processOrder(getState, order)
-      const giveAmount = Web3Utils.toBN(order.giveAmount)
-      const filled = Web3Utils.toBN(order.filled)
-      const remainingGiveAmount = giveAmount.sub(filled)
-      // skip this order if its remaining volume doesn't meet taker's minimum
-      if (remainingGiveAmount.lt(takerMinimum)) {
-        continue
-      }
-      totalSell = totalSell.add(remainingGiveAmount)
-      sellBook.push(order)
-    }
-    totalBuy = totalBuy.toString()
-    totalSell = totalSell.toString()
+    const buyBook = orderBooksResponse.data.bid.records.map(order => processOrder(getState, order))
+    const sellBook = orderBooksResponse.data.ask.records.map(order => processOrder(getState, order))
+    dispatch(loadOrderBook({ buyBook, sellBook }))
     dispatch(initializeCableSubscriptions(marketSymbol))
-    dispatch({
-      type: ORDER_BOOK_LOAD,
-      payload: { buyBook, sellBook, totalBuy, totalSell }
-    })
   };
 };
 
@@ -85,46 +49,53 @@ const updateOrderBookOrdersAsync = (newOrders) => {
   return async (dispatch, getState) => {
     const { orderBook } = getState()
 
-    let updatedBuyBook = []
-    let updatedSellBook = []
-    let updatedTotalBuy = Web3Utils.toBN(0)
-    let updatedTotalSell = Web3Utils.toBN(0)
-    for (let newOrder of newOrders) {
-      newOrder = processOrder(getState, newOrder)
-      if (newOrder.type === 'buy') {
-        updatedBuyBook.push(newOrder)
-        if (newOrder.status !== 'closed') {
-          updatedTotalBuy = updatedTotalBuy.add(Web3Utils.toBN(newOrder.takeAmount))
-        }
-      } else {
-        updatedSellBook.push(newOrder)
-        if (newOrder.status !== 'closed') {
-          updatedTotalSell = updatedTotalSell.add(Web3Utils.toBN(newOrder.giveAmount))
-        }
-      }
-    }
+    newOrders = newOrders.map(order => processOrder(getState, order))
+
+    let buyBook = newOrders.filter(order => order.type === 'buy')
     for (let order of orderBook.buyBook) {
-      const newOrder = updatedBuyBook.filter(o => o.orderHash === order.orderHash)[0]
+      const newOrder = buyBook.filter(newOrder => newOrder.orderHash === order.orderHash)[0]
       if (!newOrder) {
-        updatedBuyBook.push(order)
-        updatedTotalBuy = updatedTotalBuy.add(Web3Utils.toBN(order.takeAmount))
+        buyBook.push(order)
       }
     }
+
+    let sellBook = newOrders.filter(order => order.type === 'sell')
     for (let order of orderBook.sellBook) {
-      const newOrder = updatedSellBook.filter(o => o.orderHash === order.orderHash)[0]
+      const newOrder = sellBook.filter(newOrder => newOrder.orderHash === order.orderHash)[0]
       if (!newOrder) {
-        updatedSellBook.push(order)
-        updatedTotalSell = updatedTotalSell.add(Web3Utils.toBN(order.giveAmount))
+        sellBook.push(order)
       }
     }
-    // remove closed orders
-    updatedBuyBook = updatedBuyBook.filter(o => o.status !== 'closed')
-    updatedSellBook = updatedSellBook.filter(o => o.status !== 'closed')
-    updatedTotalBuy = updatedTotalBuy.toString()
-    updatedTotalSell = updatedTotalSell.toString()
+
+    buyBook = buyBook.filter(order => order.status !== 'closed')
+    sellBook = sellBook.filter(order => order.status !== 'closed')
+    dispatch(loadOrderBook({ buyBook, sellBook }))
+  }
+}
+
+const loadOrderBook = ({ buyBook, sellBook }) => {
+  return dispatch => {
+    let totalBuy = Web3Utils.toBN(0)
+    let totalSell = Web3Utils.toBN(0)
+    for (let order of buyBook) {
+      const takeAmount = Web3Utils.toBN(order.takeAmount)
+      const giveAmount = Web3Utils.toBN(order.giveAmount)
+      const filled = Web3Utils.toBN(order.filled)
+      const filledTake = calculateTakeAmount(filled, giveAmount, takeAmount)
+      const remainingTakeAmount = takeAmount.sub(filledTake)
+      totalBuy = totalBuy.add(remainingTakeAmount)
+    }
+    for (let order of sellBook) {
+      const giveAmount = Web3Utils.toBN(order.giveAmount)
+      const filled = Web3Utils.toBN(order.filled)
+      const remainingGiveAmount = giveAmount.sub(filled)
+      totalSell = totalSell.add(remainingGiveAmount)
+    }
+    totalBuy = totalBuy.toString()
+    totalSell = totalSell.toString()
     dispatch({
       type: ORDER_BOOK_LOAD,
-      payload: { buyBook: updatedBuyBook, sellBook: updatedSellBook, totalBuy: updatedTotalBuy, totalSell: updatedTotalSell }
+      payload: { buyBook, sellBook, totalBuy, totalSell }
     })
   }
 }
